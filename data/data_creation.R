1c691855daf516d642949064f969c5ad34105646
Michael <michael.bostwick@gmail.com>
2018-03-26 20:01:34 -0400

    basic train/test split

---
+++ data/data_creation.R
@@ -0,0 +1,21 @@
+library(caret)
+
+# This line will need to be changed to the appropriate directory"
+bank.full <- read.csv("~/Documents/STOR_565/Group project/565project/data/bank-additional-full.csv", sep=";")
+
+# Remove duration column
+bank.full <- bank.full[,-11]
+
+# Split data into training and test set
+set.seed(565)
+train.index <- createDataPartition(bank.full$y, p = .75, list = FALSE)
+train <- bank.full[ train.index,]
+test  <- bank.full[-train.index,]
+
+# Check to make sure class proportions have been maintained
+mean(bank.full$y == 'yes')
+mean(train$y == 'yes')
+mean(test$y == 'yes')
+
+write.csv(train, "~/Documents/STOR_565/Group project/565project/data/basic_train.csv")
+write.csv(test, "~/Documents/STOR_565/Group project/565project/data/basic_test.csv")
---
+++ data/readme.md
@@ -0,0 +1,9 @@
+# Data Directory
+
+### data_creation.R
+* R file to create training and test datasets
+
+## Datasets
+
+### basic_train.csv / basic_test.csv
+* Full dataset split into training and test sets (75%/25%) without any modification to features.
