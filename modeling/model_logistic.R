862113e49ae3e5fa8224f4de50f8d4af4dbee239
taottt <37820212+taottt@users.noreply.github.com>
2018-04-05 22:24:20 -0400

    4-5

--- modeling/model_logistic.R
+++ modeling/model_logistic.R
@@ -1,14 +1,33 @@
 #library(gbm)
 bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\combolog.train.csv", sep ="")
+bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_train.csv")
+bank.test<-read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_test.csv")
 head(bank.train)
 summary(bank.train)
 # Make 0/1 factor 
-bank.train$y <- as.factor(ifelse(bank.train$y =="yes",1,0))
+#bank.train$y <- ifelse(bank.train$y =="yes",1,0)
 summary(bank.train)
+bank.name=names(bank.train)
 
-logistic.bank = glm(y~.,data=bank.train, family= "binomial")
+for (i in 1:(length(bank.train)-1)){
+logistic.bank = glm(bank.train$y~bank.train[,i], data= bank.train, family= binomial)
 
-#   Warning message:
-#   glm.fit: algorithm did not converge
+#print(bank.name[i])
+print(paste("*******************",i,bank.name[i],"**********************************"))
+print(summary(logistic.bank))
+}
 
-summary(logistic.bank)
+
+log.all=glm(y~age , data= bank.train, family= binomial)
+
+summary(log.all)
+
+dim(bank.test)
+
+bank.probs=predict(log.all, newdata = bank.test, type="response")
+bank.probs[1:10]
+glm.pred=rep("yes",length(bank.test$y))
+glm.pred[glm.probs>.5]="no"
+table(glm.pred,bank.test$y)
+
+mean(glm.pred==bank.test$y)
