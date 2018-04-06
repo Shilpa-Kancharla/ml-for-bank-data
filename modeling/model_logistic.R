408c0aff58727974ce6baf6519895b0fac0dcdef
taottt <37820212+taottt@users.noreply.github.com>
2018-04-06 10:03:17 -0400

    4-6

--- modeling/model_logistic.R
+++ modeling/model_logistic.R
@@ -1,24 +1,36 @@
 #library(gbm)
-bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\combolog.train.csv", sep ="")
-bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_train.csv")
-bank.test<-read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_test.csv")
-head(bank.train)
-summary(bank.train)
+bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\combolog.train.csv", sep ="")
+bank.test <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\combolog.test.csv", sep ="")
+# bank.train <- read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_train.csv")
+# bank.test<-read.csv("C:\\Users\\tbian\\Documents\\GitHub\\565project\\data\\basic_test.csv")
+dim(bank.train)
+dim(bank.test)
+
+
+# opar<-par(lwd=2,cex=0.5,mfrow=c(5,4))
+# for (i in 1:(length(bank.train)-1)){
+# boxplot(as.factor(bank.train[,19])~bank.train[,i], ylab=colnames(bank.train)[2])
+# }
+
+
+
 # Make 0/1 factor 
 #bank.train$y <- ifelse(bank.train$y =="yes",1,0)
 summary(bank.train)
 bank.name=names(bank.train)
-
+p=0
 for (i in 1:(length(bank.train)-1)){
 logistic.bank = glm(bank.train$y~bank.train[,i], data= bank.train, family= binomial)
-
+p[i]=coef(summary(logistic.bank))[2,4]
 #print(bank.name[i])
 print(paste("*******************",i,bank.name[i],"**********************************"))
-print(summary(logistic.bank))
+print(coef(summary(logistic.bank)))
 }
 
 
-log.all=glm(y~age , data= bank.train, family= binomial)
+
+
+log.all=glm(y~.[,-2] , data= bank.train, family= binomial)
 
 summary(log.all)
 
