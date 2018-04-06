11302aaf547c72fdeeded71963a75504785a4137
Michael <michael.bostwick@gmail.com>
2018-04-06 10:45:21 -0400

    Added evaluation folder

---
+++ evaluation/comparison.R
@@ -0,0 +1,22 @@
+model.evaluate <- function(pred.probs, test.y){
+  ##### For testing ####
+  #pred.probs <- svm.probs
+  #test.y <- test.sample$y
+  ######################
+  
+  preds <- factor(pred.probs < 0.5)
+  levels(preds) <- c("no","yes")
+  (error <- mean(preds != test.y))
+  
+  roc.perf <- roc(test.y, pred.probs)
+  print(roc.perf$auc)
+  plot(roc.perf)
+}
+
+# Read in Test data
+bank.test <- read.csv("~/Documents/STOR_565/Group project/565project/data/basic_test.csv")
+
+### SVM Evaluation ###
+svm.basic.predict <- predict(svm.linear, newdata = bank.test ,  probability = TRUE)
+svm.probs <- attr(svm.predicts, "probabilities")[,1]
+model.evaluate(attr(svm.predicts, "probabilities")[,1], test.sample$y)
--- modeling/model_svm.R
+++ modeling/model_svm.R
@@ -1,19 +1,33 @@
 # SVM Model
 
 library("e1071")
-
-
+library(pROC)
 bank.train <- read.csv("~/Documents/STOR_565/Group project/565project/data/basic_train.csv")
+combo.train <- read.csv("~/Documents/STOR_565/Group project/565project/data/combolog.train.csv", sep=" ")
+set.seed(5)
+sample.index <- sample(1:nrow(combo.train),floor(0.3*nrow(combo.train)),replace=FALSE)
+train.sample <- combo.train[sample.index,]
+test.sample <- combo.train[-sample.index,]
+  
+svm.linear <- svm(y ~ ., data = train.sample, kernel = "linear", cost = 0.1, probability = TRUE)
 
-svm.linear <- svm(y ~ ., data = bank.train, kernel = "linear", cost = 0.1)
 
-
-grid <- c(0.01,0.1,1)
+grid <- c(0.1,0.2,0.3)
 set.seed(565)
 start.time <- Sys.time()
-auto.svm.linear.tune <- tune(svm, y ~ ., data = bank.train, 
+auto.svm.linear.tune <- tune(svm, y ~ ., data = train.sample, 
                              kernel = "linear", ranges = list(cost = grid),
-                             tunecontrol = tune.control(cross = 10))
+                             tunecontrol = tune.control(cross = 5))
 stop.time <- Sys.time()
+stop.time - start.time
 plot(auto.svm.linear.tune)
+
+svm.predicts <- predict(svm.linear, newdata = test.sample, probability = TRUE)
+svm.err <- mean(svm.predicts != test.sample$y)
+
+svm.roc <- roc(test.sample$y, attr(svm.predicts, "probabilities")[,1])
+plot(svm.roc)
+svm.roc$auc
+
+
 
