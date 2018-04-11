f97050c3308e518af1b6cb087e859ed8f6b38172
taottt <37820212+taottt@users.noreply.github.com>
2018-04-11 10:21:03 -0400

    10：20

--- modeling/model_logistic.R
+++ modeling/model_logistic.R
@@ -63,33 +63,67 @@ dim(y.test)
 table(y.test, log.pre)
 
 
-################################################################################################################
+######################################  train basic_ works good  ##########################################################################
 str(bank.train)
 str(bank.test)
 dim(bank.train)
 
 x.matrix.b<-model.matrix(~.,bank.train[,-20])[,-1]
 x.test.b<-model.matrix(~.,bank.test[,-20])[,-1]
-y.test.b=as.numeric(bank.test$y)
+y.test.b=bank.test$y
 
 foldid=sample(1:2,size=length(bank.train$y),replace=TRUE)
 
 start=Sys.time()
-bank.lasso.b<-cv.glmnet(x.matrix,bank.train$y, family="binomial", type.measure="class", alpha=1)
+bank.lasso.b<-cv.glmnet(x.matrix.b,bank.train$y, family="binomial", type.measure="class", alpha=1)
 stop=Sys.time()
 
 stop-start
 plot(bank.lasso.b)
-fit.b<-glmnet(x.matrix,bank.train$y, family="binomial", alpha=1,lambda = bank.lasso$lambda.1se)
+fit.b<-glmnet(x.matrix.b,bank.train$y, family="binomial", alpha=1,lambda = bank.lasso.b$lambda.1se)
 
-logistic.predict.b<-predict (fit.b, newx = x.test , type="response")
+logistic.predict.b<-predict (fit.b, newx = x.test.b , type="response")
 log.pre.b<-ifelse(logistic.predict.b<0.5,1,2)
 
 table(y.test.b, log.pre.b)
-1-mean(y.test.b==log.pre.b)
+1-mean(as.numeric(y.test.b)==log.pre.b)  #####[1] 0.1012044
 log.roc <- roc(y.test.b, attr(logistic.predict.b, "probabilities")[,1])
 plot(log.roc)
 log.roc$auc
+
+
+########################################## modified_ works good ########################################################################
+
+str(bank.train.mod)
+str(bank.test.mod)
+dim(bank.train.mod)
+
+x.matrix.m<-model.matrix(~.,bank.train.mod[,-14])[,-1]
+x.test.m<-model.matrix(~.,bank.test.mod[,-14])[,-1]
+y.test.m=bank.test.mod$y
+dim(x.matrix.m)
+
+foldid.m=sample(1:2,size=length(bank.train.mod$y),replace=TRUE)
+
+start=Sys.time()
+bank.lasso.m<-cv.glmnet(x.matrix.m,bank.train.mod$y, family="binomial", type.measure="class", alpha=1)
+stop=Sys.time()
+
+stop-start
+plot(bank.lasso.m)
+fit.m<-glmnet(x.matrix.m,bank.train.mod$y, family="binomial", alpha=1,lambda = bank.lasso.m$lambda.1se)
+
+logistic.predict.m<-predict (fit.m, newx = x.test.m , type="response")
+log.pre.m<-ifelse(logistic.predict.m<0.5,"no","yes")
+
+table(y.test.m, log.pre.m)
+1-mean(y.test.m==log.pre.m)   #####[1] 0.1007187
+log.roc <- roc(y.test.b, attr(logistic.predict.b, "probabilities")[,1])
+plot(log.roc)
+log.roc$auc
+
+
+
 
 ##################################################################################################################
 # Make 0/1 factor 
